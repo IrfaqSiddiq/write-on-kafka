ff006885425a9a3d9d5cb30a43e57fb728f9a24e
IrfaqSiddiq <mohammed.irfaq786@gmail.com>
2023-11-26 14:20:17 +0530

    pushed code to write message on kafka with test topic

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	// Create a new Kafka writer.
+	w := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{"localhost:9092"},
+		Topic:   "test-topix",
+	})
+	defer w.Close()
+
+	// Write a message.
+	err := w.WriteMessages(context.Background(),
+		kafka.Message{
+			Key:   []byte("Key-A"),
+			Value: []byte("Hello khan nabi!"),
+		},
+	)
+
+	if err != nil {
+		fmt.Println("***Error", err)
+		panic(err)
+	}
+
+	fmt.Println("Message sent")
+}
